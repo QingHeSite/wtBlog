942bca66f04d9fbe13872aa0c7d7cacab2561b28
wangtao0910 <Wt123412>
2020-07-21 14:59:41 +0800

    feat:修改评论样式

--- docs/.vuepress/components/ValineComment.vue
+++ docs/.vuepress/components/ValineComment.vue
@@ -3,3 +3,9 @@
        <div id="valine-vuepress-comment"></div> 
     </div>
 </template>
+<style lang="scss">
+    #valine-vuepress-comment{
+        width: 840px;
+        margin: 0 auto;
+    }
+</style>
