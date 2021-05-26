c642e00778b7abe79fe6627b7cc8af96de9dbf0a
wangtao0910 <Wt123412>
2021-05-26 15:08:10 +0800

    新增图片压缩

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,6 +28,7 @@ module.exports = {
                   '/core/error',//常见错误解决
                   '/core/performance',//网页性能
                   '/core/typescript',// ts
+                  `/core/img`
                 ]
               }
 
