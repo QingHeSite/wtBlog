21b7ac442fbbd5a1737a1cc8fa648be1f5c232b3
wangtao0910 <Wt123412>
2021-07-16 10:35:15 +0800

    1

--- docs/.vuepress/components/Index.vue
+++ docs/.vuepress/components/Index.vue
@@ -5,7 +5,8 @@
         <p>👈更多博客可以打开左侧栏哦</p>
         <p>有兴趣可以一起写博客交流经验呢🤔</p>
         <span>wechat👇</span>
-        <div><img src="../assets/imgs/wt_wechat.jpg" width="300" alt=""></div>
+        <button class="open-btn" @click="showWechat">展开微信码</button>
+        <div v-if="showWechatStatus"><img src="../assets/imgs/wt_wechat.jpg" width="300" alt=""></div>
     </div>
 </template>
 <script>
@@ -13,10 +14,31 @@ export default {
     name: 'indexPage',
     data: function() {
         return {
-            page: 'index-'
+            page: 'index-',
+            showWechatStatus: false
+        }
+    },
+    methods: {
+        showWechat() {
+            this.showWechatStatus = true
         }
     },
     mounted() {
     }
 }
 </script>
+<style lang="scss" scoped>
+    .open-btn{
+        width: 90px;
+        height: 28px;
+        line-height: 26px;
+        padding: 0;
+        font-weight: 600;
+        cursor: pointer;
+        margin-left: 20px;
+        transition: all .3s;
+        &:hover{
+            transform: scale(1.2);
+        }
+    }
+</style>
