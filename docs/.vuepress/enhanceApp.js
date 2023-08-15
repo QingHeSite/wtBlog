a813095520b6add76a14b95e077b135f2c317817
wangtao <skymrwt@gmail.com>
2023-08-15 14:20:29 +0800

    百度统计

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -7,13 +7,13 @@ export default ({
     Vue.mixin({
      mounted() {
          if(!_hmt){
-            var _hmt = _hmt || [];
-            (function() {
-              var hm = document.createElement("script");
-              hm.src = "https://hm.baidu.com/hm.js?7d3fc4f03f8e7f5c2dd0cc223a2c8c8a";
-              var s = document.getElementsByTagName("script")[0]; 
-              s.parentNode.insertBefore(hm, s);
-            })();
+             var _hmt = _hmt || [];
+             (function() {
+                 var hm = document.createElement("script");
+                 hm.src = "https://hm.baidu.com/hm.js?2bf89a1147ecc9984519af24104216e0";
+                 var s = document.getElementsByTagName("script")[0];
+                 s.parentNode.insertBefore(hm, s);
+             })();
          }
      }
     })
