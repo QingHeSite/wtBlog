5392a9a2c595887e3efd7cbf9271dc3e57f69cd0
wangtao <skymrwt@gmail.com>
2023-08-16 10:29:50 +0800

    stat

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -10,7 +10,7 @@ export default ({
              var _hmt = _hmt || [];
              (function() {
                  var hm = document.createElement("script");
-                 hm.src = "https://hm.baidu.com/hm.js?2bf89a1147ecc9984519af24104216e0";
+                 hm.src = "https://hm.baidu.com/hm.js?20807e9f1461c920ec76c5c921f6dbfb";
                  var s = document.getElementsByTagName("script")[0];
                  s.parentNode.insertBefore(hm, s);
              })();
